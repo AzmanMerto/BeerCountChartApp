431e5771040cfe4f5bc4b45d5dd2e3236e4ad119
NomoteteS <nomotetes.onetrue@icloud.com>
2022-12-30 21:49:03 +0200

    Initial commit

---
+++ Responsive/ResponsiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResponsiveApp.swift
+//  Responsive
+//
+//  Created by NomoteteS on 30.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ResponsiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
